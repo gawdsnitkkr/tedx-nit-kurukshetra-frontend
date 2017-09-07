413e212ab931cae5fb424d6c782b615cf049d8ae
rishabh2609 <rishabhchanana97@gmail.com>
2017-09-07 23:27:38 +0530

    complete ui change

--- js/main.js
+++ js/main.js
@@ -1,64 +1,26 @@
-
-function splittext() {
-	var i = -2;
-	var interval = setInterval(
-		function() {
-			i++;
-			var obj = '#' + i; 
-			$(obj).removeClass('transformation_light');
-			$(obj).addClass('transformation');
-		
-			obj = '#' + (i - 1);
-			$(obj).addClass('transformation_light');
-			$(obj).removeClass('transformation');
-
-
-			obj = '#' + (i + 1);
-			$(obj).addClass('transformation_light');
-			$(obj).removeClass('transformation');
-
-			obj = '#' + (i - 2);
-			$(obj).removeClass('transformation_light');
-			$(obj).removeClass('transformation');
-			if(i == 12){
-				clearInterval(interval);
-				start();
-			}
-		}
-		,150
-	);	
+function swiwel() {
+    TweenMax.to("#college-name", 2.5, {
+            rotationX: 0,
+            delay: 0.5,
+            ease: Elastic.easeOut.config(1, 0.2)
+        }
+    );
+    TweenMax.staggerFrom(".social-icons img", 1, {
+            scale: 2,
+            opacity: 0,
+            delay: 1,
+            ease: Bounce.easeOut
+        },
+        0.2
+    );
+    TweenMax.staggerFrom("span", 0.7, {
+            opacity: 0,
+            delay: 2,
+            repeat: 3,
+            yoyo: true,
+            ease: Bounce.easeOut
+        },
+        0.1
+    );
 }
-function start() {
-	$('body').css('background', 'black');
-	$('#splash').addClass('hidden');
-	TweenMax.to('#coming_soon', 0.5, {
-		opacity: 1
-	}
-	);
-	TweenMax.to('#main_info_cont', 1, {
-		opacity: 1,
-		onComplete: splittext
-	}
-	);
-}
-
-function full_splash() {
-	var splash = $('#splash');
-	TweenMax.to(splash, 0.5,  {
-		scale: 250,
-		onComplete: start
-	}
-	);
-}
-
-function splash_screen() {
-	var splash = $('#splash');
-	TweenMax.to(splash, 0.2, {
-		opacity: 0,
-		repeat: 5,
-		yoyo: true,
-		onComplete: full_splash
-	}
-	);
-}
-splash_screen();
+swiwel();
